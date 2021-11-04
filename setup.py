43e93d6179227e19c842020739165b188e739ca2
Dave Brown <davebrown@Daves-iMac.local>
2021-11-04 22:28:39 +0000

    Version 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='ArtistSongsLyrics',  
-     version='0.3',
+     version='0.4',
      author="Dave Brown",
      author_email="dave.k.flowers@gmail.com",
      description="Oooo the way you move me baby",
